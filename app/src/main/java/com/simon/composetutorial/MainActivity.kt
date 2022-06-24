f8a34f0eb18c0f52f5eae90802918816f6a60365
Simon.zhang <349283970@qq.com>
2022-06-24 15:29:33 +0800

    使用Row排列，并添加Image控件

--- app/src/main/java/com/simon/composetutorial/MainActivity.kt
+++ app/src/main/java/com/simon/composetutorial/MainActivity.kt
@@ -3,13 +3,16 @@ package com.simon.composetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.simon.composetutorial.ui.theme.ComposeTutorialTheme
 
@@ -26,9 +29,15 @@ data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    Column {
-        Text(text = msg.author)
-        Text(text = msg.body)
+    Row {
+        Image(
+            painterResource(id = R.drawable.profile),
+            contentDescription = "contact profile picture"
+        )
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
     }
 }
 
