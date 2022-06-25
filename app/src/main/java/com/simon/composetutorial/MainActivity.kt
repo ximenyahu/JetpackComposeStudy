b5c8fff1f83640696157198d7ebdb10f8c8e4b18
Simon.zhang <349283970@qq.com>
2022-06-25 16:03:51 +0800

    isExpanded、Surface、MutableStateOf、maxLines使用

--- app/src/main/java/com/simon/composetutorial/MainActivity.kt
+++ app/src/main/java/com/simon/composetutorial/MainActivity.kt
@@ -4,19 +4,29 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.simon.composetutorial.data.MessageRepository
 import com.simon.composetutorial.ui.theme.ComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +34,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme {
-                MessageCard(Message(author = "Android", body = "Jetpack Compose is great!"))
+                Conversation(MessageRepository.messages)
             }
         }
     }
@@ -33,10 +43,19 @@ class MainActivity : ComponentActivity() {
 data class Message(val author: String, val body: String)
 
 @Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Composable
 fun MessageCard(msg: Message) {
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
-            painterResource(id = R.drawable.profile),
+            painterResource(id = R.drawable.contact_profile_picture),
             contentDescription = "contact profile picture",
             modifier = Modifier
                 .size(40.dp)
@@ -44,18 +63,30 @@ fun MessageCard(msg: Message) {
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column {
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface)
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colors.secondaryVariant,
                 style = MaterialTheme.typography.subtitle2
             )
             Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
                 Text(
                     text = msg.body,
                     style = MaterialTheme.typography.body2,
-                    modifier = Modifier.padding(4.dp)
+                    modifier = Modifier.padding(4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
                 )
             }
         }
@@ -71,6 +102,6 @@ fun MessageCard(msg: Message) {
 @Composable
 fun PreviewMessageCard() {
     ComposeTutorialTheme {
-        MessageCard(Message("Android", "Hey, take a look at Jetpack Compose. it's great!"))
+        Conversation(messages = MessageRepository.messages)
     }
 }
---
+++ app/src/main/java/com/simon/composetutorial/data/MessageRepository.kt
@@ -0,0 +1,45 @@
+package com.simon.composetutorial.data
+
+import com.simon.composetutorial.Message
+
+object MessageRepository {
+    val messages = listOf(
+        Message(author = "C", body = "Jetpack is great!"),
+        Message(author = "C++", body = "Jetpack is great!"),
+        Message(author = "C#", body = "Jetpack is great!"),
+        Message(
+            author = "Java",
+            body = "只包含一条消息的聊天会略显孤单，因此请更改对话，使其包含多条消息。您需要创建一个可显示多条消息的 Conversation 函数。对于此用例，请使用 Compose 的 LazyColumn 和 LazyRow。这些可组合项只会呈现屏幕上显示的元素，因此，对于较长的列表，使用它们会非常高效。"
+        ),
+        Message(author = "pathy", body = "Jetpack is great!"),
+        Message(
+            author = "Kotlin",
+            body = "只包含一条消息的聊天会略显孤单，因此请更改对话，使其包含多条消息。您需要创建一个可显示多条消息的 Conversation 函数。对于此用例，请使用 Compose 的 LazyColumn 和 LazyRow。这些可组合项只会呈现屏幕上显示的元素，因此，对于较长的列表，使用它们会非常高效。"
+        ),
+        Message(author = "C", body = "Jetpack is great!"),
+        Message(author = "C++", body = "Jetpack is great!"),
+        Message(author = "C#", body = "Jetpack is great!"),
+        Message(
+            author = "Java",
+            body = "只包含一条消息的聊天会略显孤单，因此请更改对话，使其包含多条消息。您需要创建一个可显示多条消息的 Conversation 函数。对于此用例，请使用 Compose 的 LazyColumn 和 LazyRow。这些可组合项只会呈现屏幕上显示的元素，因此，对于较长的列表，使用它们会非常高效。"
+        ),
+        Message(author = "pathy", body = "Jetpack is great!"),
+        Message(
+            author = "Kotlin",
+            body = "只包含一条消息的聊天会略显孤单，因此请更改对话，使其包含多条消息。您需要创建一个可显示多条消息的 Conversation 函数。对于此用例，请使用 Compose 的 LazyColumn 和 LazyRow。这些可组合项只会呈现屏幕上显示的元素，因此，对于较长的列表，使用它们会非常高效。"
+        ),
+        Message(author = "C", body = "Jetpack is great!"),
+        Message(author = "C++", body = "Jetpack is great!"),
+        Message(author = "C#", body = "Jetpack is great!"),
+        Message(
+            author = "Java",
+            body = "只包含一条消息的聊天会略显孤单，因此请更改对话，使其包含多条消息。您需要创建一个可显示多条消息的 Conversation 函数。对于此用例，请使用 Compose 的 LazyColumn 和 LazyRow。这些可组合项只会呈现屏幕上显示的元素，因此，对于较长的列表，使用它们会非常高效。"
+        ),
+        Message(author = "pathy", body = "Jetpack is great!"),
+        Message(
+            author = "Kotlin",
+            body = "只包含一条消息的聊天会略显孤单，因此请更改对话，使其包含多条消息。您需要创建一个可显示多条消息的 Conversation 函数。对于此用例，请使用 Compose 的 LazyColumn 和 LazyRow。这些可组合项只会呈现屏幕上显示的元素，因此，对于较长的列表，使用它们会非常高效。"
+        )
+    )
+
+}
